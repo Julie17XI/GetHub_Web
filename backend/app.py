2f39d2cd2e9d68c4e144a52aa70828771d18b102
Julie17XI <xi.tang717@gmail.com>
2022-09-25 17:59:12 -0500

    finished web scarping

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_restful import Api
 from flask_cors import CORS #comment this on deployment
+from web_scraper import user_info
 
 app = Flask(__name__)
 CORS(app) #comment this on deployment
@@ -11,7 +12,8 @@ api = Api(app)
 def home():
     if request.method == "POST":
         username = request.json['username']
-        return username
+        info = user_info(username)
+        return info
     return "Hello World!"
 
 if __name__ == '__main__':
--- backend/web_scraper.py
+++ backend/web_scraper.py
@@ -1,28 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 
-website1 = requests.get('https://github.com/Julie17XI')
-soup1 = BeautifulSoup(website1.content, 'html.parser')
-website2 = requests.get('https://github.com/Julie17XI?page=1&tab=repositories')
-soup2 = BeautifulSoup(website2.content, 'html.parser')
-user = soup1.find('strong').string.strip()
-repo_nums = soup1.find('span', class_='Counter').string.strip()
-last_yr_contribution_text = soup1.find('h2', class_='f4 text-normal mb-2').string.strip()
-last_yr_contribution = re.findall("\d+", last_yr_contribution_text)[0]
-print(user)
-print(repo_nums)
-print(last_yr_contribution)
-print('------------------------------')
-repos = soup2.find_all('li', itemprop='owns')
-for repo in repos:
-    repo_name = repo.find('a', itemprop='name codeRepository')
-    repo_lang = repo.find('span', itemprop='programmingLanguage')
-    repo_description = repo.find('p', itemprop='description')
-    if repo_name:
-        print(repo_name.string.strip())
-    if repo_lang:
-        print(repo_lang.string.strip())
-    if repo_description:
-        print(repo_description.string.strip())
-    print('------------------------------')
+def user_info(username):
+
+    url1 = 'https://github.com/' + username
+    url2 = 'https://github.com/' + username + '?page=1&tab=repositories'
+    website1 = requests.get(url1)
+    website2 = requests.get(url2)
+    soup1 = BeautifulSoup(website1.content, 'html.parser')
+    soup2 = BeautifulSoup(website2.content, 'html.parser')
+
+    user = soup1.find('strong').string.strip()
+    repo_nums = soup1.find('span', class_='Counter').string.strip()
+    last_yr_contribution_text = soup1.find('h2', class_='f4 text-normal mb-2').string.strip()
+    last_yr_contribution = re.findall("\d+", last_yr_contribution_text)[0]
+
+    repos = soup2.find_all('li', itemprop='owns')
+    repos_info = []
+    for repo in repos:
+        repo_info = {}
+        repo_name = repo.find('a', itemprop='name codeRepository')
+        repo_lang = repo.find('span', itemprop='programmingLanguage')
+        repo_description = repo.find('p', itemprop='description')
+        if repo_name:
+            repo_info["repo_name"] = repo_name.string.strip()
+        if repo_lang:
+            repo_info["repo_lang"] = repo_lang.string.strip()
+        if repo_description:
+            repo_info["repo_description"] = repo_description.string.strip()
+        repos_info.append(repo_info)
+
+    info = {
+            "user": user,
+            "repo_nums": repo_nums,
+            "last_yr_contribution": last_yr_contribution,
+            "repos_info": repos_info
+            }
+
+    return json.dumps(info)
