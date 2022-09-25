e71eec11e146c2c51140565c30248af82b466aeb
Julie17XI <xi.tang717@gmail.com>
2022-09-25 17:25:02 -0500

    connected react app and flask app

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,18 @@
 from flask import Flask
+from flask import request
 from flask_restful import Api
 from flask_cors import CORS #comment this on deployment
-from HelloApiHandler import HelloApiHandler
 
 app = Flask(__name__)
 CORS(app) #comment this on deployment
 api = Api(app)
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
-
-api.add_resource(HelloApiHandler, '/flask/hello')
+@app.route("/", methods = ["GET", "POST"])
+def home():
+    if request.method == "POST":
+        username = request.json['username']
+        return username
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
