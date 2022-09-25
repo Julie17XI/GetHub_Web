0e1433a06dbfe94fbd481f78c665d18704a6fa4c
Julie17XI <xi.tang717@gmail.com>
2022-09-25 12:37:53 -0500

    finished react components without styling

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse
+from flask import Flask
+from flask_restful import Api
 from flask_cors import CORS #comment this on deployment
-from backend.HelloApiHandler import HelloApiHandler
+from HelloApiHandler import HelloApiHandler
 
 app = Flask(__name__)
 CORS(app) #comment this on deployment
@@ -12,3 +12,6 @@ def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
 api.add_resource(HelloApiHandler, '/flask/hello')
+
+if __name__ == '__main__':
+    app.run(debug=True)
