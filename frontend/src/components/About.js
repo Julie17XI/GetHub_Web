1692c07a5489eaf5c364de1ceafb10f1a2e8233e
Julie17XI <xi.tang717@gmail.com>
2022-10-14 10:26:50 -0500

    changed Landing page description.

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -22,7 +22,7 @@ const About = () => {
                             Built For  |
                     </h3>
                     <span class="blue-text">
-                            Joyful Discovery...We invite you to explore <span class="bold">GitHub</span> repositories by entering a GitHub username. Search today!
+                            Joyful Discovery...We invite you to learn about a <span class="bold">GitHub</span> user and his/her repositories by entering a GitHub username. Search today!
                     </span>
                 </>
             </>
